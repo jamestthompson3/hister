46ee6c193df8e7170fca3d271962a6fb0bf23df5
Adam Tauber <asciimoo@gmail.com>
2026-03-02 23:15:02 +0100

    [enh] add env var to define custom base path

--- webui/app/svelte.config.js
+++ webui/app/svelte.config.js
@@ -1,8 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
+const baseDir = process.env.BASE_PATH || '';
+
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
+    paths: {
+      base: baseDir,
+    },
     adapter: adapter({
       pages: 'build',
       assets: 'build',
