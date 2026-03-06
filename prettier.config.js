8712381ee9b86ca525b0223952163870425853e9
Adam Tauber <asciimoo@gmail.com>
2026-03-06 14:53:30 +0100

    [enh] add trailingComma option to js formatter config

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: [
-    "prettier-plugin-svelte",
-    "prettier-plugin-tailwindcss", // MUST COME LAST
+    'prettier-plugin-svelte',
+    'prettier-plugin-tailwindcss', // MUST COME LAST
   ],
-  overrides: [{ files: "*.svelte", options: { parser: "svelte" } }],
+  overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }],
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
   semi: true,
   singleQuote: true,
+  trailingComma: 'all',
 };
